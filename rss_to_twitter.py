6aa354283178db0ae48eb7059fa8999cec189602
mattstruble <mattstruble@outlook.com>
2021-03-12 21:46:57 -0500

    Updated rss_reader saved data in order to add a redundancy in case of the etag misbehaving. Now also stores last posted message id and ignores all older messages.

--- rss_to_twitter.py
+++ rss_to_twitter.py
@@ -3,7 +3,7 @@ from social_manager.twitter_handler import TwitterHandler
 
 
 def tweet_items(feed_reader: RSSReader, handler):
-    for item in feed_reader.feed_items:
+    for item in feed_reader.auto_saving_items_generator():
         message = handler.format_message(item.summary, item.title, item.link)
         handler.post(message)
 
--- social_manager/rss_reader.py
+++ social_manager/rss_reader.py
@@ -1,4 +1,5 @@
 import os
+import json
 from copy import deepcopy
 from dataclasses import dataclass
 
@@ -10,6 +11,7 @@ from .utils import get_data_dir
 
 @dataclass
 class FeedItem:
+    id: int
     title: str
     summary: str
     link: str
@@ -20,29 +22,60 @@ class RSSReader:
         self.config = ConfigReader("configs/rss.cfg")
 
         self.feed_url = self.config["feed_url"]
-        self.data_file = os.path.join(get_data_dir(), "rss.dat")
+        self.data_file = os.path.join(get_data_dir(), "rss.json")
 
         # Load in previous RSS etag to only collect newest feed information
         etag = ""
         if os.path.exists(self.data_file):
             with open(self.data_file, "r") as f:
-                etag = f.readlines()[0]
+                self.saved_data = json.load(f)
+        else:
+            self.saved_data = {"etag": "", "last_id": 0}
 
-        self.feed_parser = feedparser.parse(self.feed_url, etag=etag)
+        print(self.saved_data)
+
+        self.feed_parser = feedparser.parse(self.feed_url, etag=self.saved_data["etag"])
 
         self.feed_items = []
         if self.feed_parser:
             self._parse_feed_items()
 
-        # Save etag to file
+        self._filter_feed_items()
+
+        self.saved_data["etag"] = self.feed_parser.etag
+
+    def save(self):
         with open(self.data_file, "w") as f:
-            f.write(self.feed_parser.etag)
+            json.dump(self.saved_data, f)
+
+    def auto_saving_items_generator(self):
+        for feed_item in self.feed_items:
+            try:
+                yield feed_item
+                self.saved_data["last_id"] = feed_item.id
+            finally:
+                self.save()
+
+    def _filter_feed_items(self):
+        self.feed_items.sort(key=lambda x: x.id)
+
+        i = 0
+        for feed_item in self.feed_items:
+            print(feed_item.id)
+            if feed_item.id > int(self.saved_data["last_id"]):
+                break
+            i+=1
+
+        self.feed_items = self.feed_items[i:]
+
+        print(self.feed_items)
 
     def _parse_feed_items(self):
         for item in self.feed_parser["items"]:
+            id = int(item['id'][11:])
             title = item["title"]
             link = item["links"][0]["href"]
             summary = item["summary"]
 
-            feed_item = FeedItem(title=title, summary=summary, link=link)
+            feed_item = FeedItem(id = id, title=title, summary=summary, link=link)
             self.feed_items.append(deepcopy(feed_item))
